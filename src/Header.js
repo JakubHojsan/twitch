36df73f9bdb9356810cb8db2ebf269984cb21d83
Jakub Hojsan <jakubhojsan@g.ucla.edu>
2021-10-19 14:44:46 -0700

    Added boxes, reworked alignment in header, and added buttons.

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,37 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Icons } from "@material-ui/core";
 import React from "react";
 import './App.css';
 import logo from'./logo.png';
 import SearchBar from "material-ui-search-bar";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 
-
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+/>
 
 export default function Header() {
     const displayDesktop = () => {
       return (
         <Toolbar>
           <div className='left'>
-            <img  src={logo} alt='test' width='35px' height='35px'/>
-
+            <Stack direction='row' spacing={3}>
+              <img  src={logo} alt='test' width='35px' height='35px'/>
+              <Button
+                variant="text">
+                  Browse
+                </Button>
+                <Button 
+                variant="text">
+                  More
+                </Button>
+            </Stack>
           </div>
 
 
@@ -34,10 +50,14 @@ export default function Header() {
             <Stack spacing={1} direction="row">
                 <Button 
                 style={{backgroundColor: "#D3D3D3"}}
-                variant="contained">Log in</Button>
+                variant="contained">
+                  Log in
+                </Button>
                 <Button 
                 style={{backgroundColor: "#6441a5"}}
-                variant="contained">Sign up</Button>
+                variant="contained">
+                    Sign up
+                </Button>
             </Stack>
 
 
